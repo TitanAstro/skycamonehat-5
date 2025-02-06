6b9eda7d58cf2034441c8acca71a214b20569373
AstroImagineer <10076241+astroimagineer@users.noreply.github.com>
2025-02-06 12:19:22 +0100

    Update blinkatest.py
    SkyCamOne HAT does not use SPI, disable test as it is irrelevant.

--- blinkatest.py
+++ blinkatest.py
@@ -13,7 +13,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 print("I2C ok!")
 
 # Try to create an SPI device
-spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
-print("SPI ok!")
+#spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+#print("SPI ok!")
 
 print("done!")
